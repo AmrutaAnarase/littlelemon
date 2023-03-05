a798ef2bb2c53961d48c4c152ade57dbcd9f1a6f
Amruta Anarase <amruta@creativegalileo.com>
2023-03-05 23:09:49 +0530

    Initial Commit

---
+++ littlelemon_app/littlelemon_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  littlelemon_appApp.swift
+//  littlelemon_app
+//
+//  Created by amruta on 05/03/23.
+//
+
+import SwiftUI
+
+@main
+struct littlelemon_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
